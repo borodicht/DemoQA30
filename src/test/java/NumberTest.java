99e3796db4f44c399d5f7cdeb89bd031af3ae015
Borodich Timofei <borodich.timofei@gmail.com>
2025-04-15 16:35:09 +0300

    Initial commit

---
+++ src/test/java/NumberTest.java
@@ -0,0 +1,65 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NumberTest {
+
+    /*
+    НАПИСАТЬ МЕТОД, КОТОРЫЙ ПРИНИМАЕТ ЧИСЛО И ВОЗВРАЩВЕТ СТРОКУ
+    УСЛОВИЕ:
+    ЕСЛИ ЧИСЛО КРАТНО 3, ТО ВЕРНУТЬ T
+    ЕСЛИ ЧИСЛО КРАТНО 5, ТО ВЕРНУТЬ М,
+    ЕСЛИ ЧИСЛО КРАТНО и 3 и 5, ТО ВЕРНУТЬ TMS
+
+     3, 5, 15, 4, -5, 0
+     */
+
+    public static String checkNumber(int number) {
+        if (number % 3 == 0) {
+            return "T";
+        }
+        else if (number % 5 == 0) {
+            return "M";
+        } else if (number % 3 == 0 &&  number % 5 == 0) {
+            return "TMS";
+        }
+        else {
+            return "blablabla";
+        }
+    }
+
+    @Test
+    public void test() {
+        String result = checkNumber(3);
+        Assert.assertEquals(result, "T");
+    }
+
+    @Test
+    public void test1() {
+        String result = checkNumber(5);
+        Assert.assertEquals(result, "M");
+    }
+
+    @Test
+    public void test2() {
+        String result = checkNumber(15);
+        Assert.assertEquals(result, "TMS");
+    }
+
+    @Test
+    public void test3() {
+        String result = checkNumber(4);
+        Assert.assertEquals(result, "blablabla");
+    }
+
+    @Test
+    public void test4() {
+        String result = checkNumber(-5);
+        Assert.assertEquals(result, "M");
+    }
+
+    @Test
+    public void test5() {
+        String result = checkNumber(0);
+        Assert.assertEquals(result, "blablabla");
+    }
+}
